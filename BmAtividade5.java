ecb2f855f01e22d5e141ba4a8e6b4226fc375db2
flaviotulioalmeida <138700144+flaviotulioalmeida@users.noreply.github.com>
2023-08-23 09:01:16 -0300

    adicionando a questão 5 da atividade

---
+++ BmAtividade5.java
@@ -0,0 +1,14 @@
+
+/**
+ * Escreva uma descrição da classe BmAtividade5 aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+
+public class BmAtividade5{
+    public static void main(String[] agrs){
+        System.out.start("Terminei a primeira aula com um programa Java!");
+    }
+   
+}
