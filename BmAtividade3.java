f795d9943260fcb089e8e8d8e5ab9c9618098e50
flaviotulioalmeida <138700144+flaviotulioalmeida@users.noreply.github.com>
2023-08-23 08:45:42 -0300

    Adicionando questão 3

---
+++ BmAtividade3.java
@@ -0,0 +1,12 @@
+
+/**
+ * Escreva uma descrição da classe BmAtividade3 aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class BmAtividade3{
+    public static void main(String[] agrs){
+        System.out.print("Terminei a primeira aula com um programa Java!");
+    }
+}
