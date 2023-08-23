c9c162aefc39de8d04b9a86cf89c09abd988264f
flaviotulioalmeida <138700144+flaviotulioalmeida@users.noreply.github.com>
2023-08-23 09:09:21 -0300

    Add files via upload

---
+++ BmAtividade7.java
@@ -0,0 +1,14 @@
+
+/**
+ * Escreva uma descrição da classe BmAtividade7 aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+PUBLIC CLASS BmAtividade7{
+    PUBLIC STATIC VOID MAIN(STRING[] AGRS){
+        SYTEM.OUT.PRINT("TERMINEI PRIMEIRA AULA COM UM PROGRAMA JAVA !");
+    }
+}
+
+   
